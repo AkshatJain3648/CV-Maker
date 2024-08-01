f312aeff9718349f79b507ec67021379df3c944f
AkshatJain3648 <akshatjain3648@gmail.com>
2024-08-01 18:36:55 +0530

    Changes in design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import FirstForm from "./components/FirstForm"
 import Navbar from "./components/Navbar"
+import './Form.css'
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar/>
       <FirstForm/>
-    </>
+    </div>
   )
 }
 
--- src/components/FirstForm.jsx
+++ src/components/FirstForm.jsx
@@ -2,22 +2,44 @@ import React from 'react'
 
 const FirstForm = () => {
     return (
-        <div className='flex justify-center '>
-            <form action="action" className='form w-[80%] bg-white rounded- text-center flex flex-col'>
-                <h2 className='text-3xl'>General Info.</h2>
+        <div className='flex justify-center h-[100vh] bg-[#1d1e22]'>
+            <form action="action" className='form px-3 py-6 w-[80%] bg-[#15172b] rounded-md text-center flex flex-col mt-[5%]'>
+            <h2 class="title text-3xl text-white font-">Welcome</h2>
+            <h3 class="subtitle">Let's create your CV!</h3>
 
-                <div className=" flex flex-col left bg-yellow-300 mt-[4%] w-[30%]">
+                <div className="left flex flex-col border border-solid border-yellow-600 md:w-[50%]  items-center py-3 px-2">
 
-                    <label className='cursor-pointer'><img src="https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg" alt="profile" className='w-[100px] rounded-full'/><input type="file" accept="image/*" id="photo" className='hidden'/></label>
+                    <label className='cursor-pointer'><img src="https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg" alt="profile" className='w-[100px] rounded-full' /><input type="file" accept="image/*" id="photo" className='hidden' /></label>
 
-                    <button className="text-red-400 font-bold" id='delete'>Delete X</button>
+                    {/* <button className="text-red-400 font-bold" id='delete'>Delete X</button> */}
                 </div>
-                <div className="right">
+                <div className="right flex flex-col md:w-[50%]">
                     <form action="">
-                        <input type="text" name="firstName" id="" />
-                        <input type="text" name="lastName" id="" />
-                        <input type="phone" name="Phone" id="" />
-                        <input type="email" name="Email" id="" />
+                        <div class="input-container ic1">
+                            <input id="firstname" class="input" type="text" placeholder=" " />
+                            <div class="cut"></div>
+                            <label for="firstname" class="placeholder">First name</label>
+                        </div>
+                        <div class="input-container ic2">
+                            <input id="lastname" class="input" type="text" placeholder=" " />
+                            <div class="cut"></div>
+                            <label for="lastname" class="placeholder">Last name</label>
+                        </div>
+                        <div class="input-container ic2">
+                            <input id="email" class="input" type="text" placeholder=" " />
+                            <div class="cut cut-short"></div>
+                            <label for="email" class="placeholder">Email</label>
+                        </div>
+                        <div class="input-container ic2">
+                            <input id="email" class="input" type="text" placeholder=" " />
+                            <div class="cut cut-short"></div>
+                            <label for="email" class="placeholder">Phone</label>
+                        </div>
+                        <div class="input-container ic2">
+                            <input id="address" class="input" type="address" placeholder=" " />
+                            <div class="cut cut-short"></div>
+                            <label for="address" class="placeholder">Address</label>
+                        </div>
                     </form>
                 </div>
             </form>
